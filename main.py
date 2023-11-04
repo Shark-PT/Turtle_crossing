a78c749cbf92f2a292b9a47057cbd5d537b5eb53
SharkPT <jccreis@hotmail.com>
2023-11-04 19:15:32 +0000

    Fim do movimento dos carros

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player = Player()
-car = CarManager()
+car_manager = CarManager()
 
 screen.listen()
 screen.onkeypress(player.go_up, "Up")
@@ -20,6 +20,9 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
 
     if player.ycor() > 280:
         player.goto(0, -200)
